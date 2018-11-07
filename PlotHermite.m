40f8e1b9ef5d5f9cfd8ca909e5357f1deebfaf8c
ksonod <44776642+ksonod@users.noreply.github.com>
2018-11-07 13:22:08 +0100

    Add files via upload

--- PlotHermite.m
+++ PlotHermite.m
@@ -1,9 +1,9 @@
 %Plot the Hermite polynomial H_v(x) as a function of x. 
-%You can adjust the part of this program labeld by [1].
+%You can adjust the part of this program labeld by _1_.
 
-%[1]%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%_1_%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 xmin=-10; %The minimum value of x. The maximum value of x is given by -xmin
-v=5; % v-th order Hermite polynomial. 
+v=0; % v-th order Hermite polynomial. 
 %The value v should be zero or positive integer.
 
 %optional
@@ -12,14 +12,21 @@ dx=0.001; %segment for x.
 
 xmax=-xmin; %The maximum value of x.
 
-iter=(xmax-xmin)/dx;
-x=zeros(iter,1);
-Herm=zeros(iter,1);
+if 0<xmin %avoid an error
+    disp('xmin should be a negative value!');
+elseif 0<abs(v-round(v)) % avoid an error
+    disp('The value of v should be zero or a positive integer!');
+else %calculation
+    iter=(xmax-xmin)/dx;
+    x=zeros(iter,1);
+    Herm=zeros(iter,1);
 
-for j=1:1:iter
-    x(j,1)=xmin+(j-1)*dx ;
-    Herm(j,1)=Hermite(v,x(j,1));
+    for j=1:1:iter
+        x(j,1)=xmin+(j-1)*dx ;
+        Herm(j,1)=Hermite(v,x(j,1));
+    end
+
+    plot(x,Herm);
 end
 
-plot(x,Herm);
 clear xmin xmax dx v iter j;
